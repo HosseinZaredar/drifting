a6b6f7e50242c1e302c2a4fc5e0409667bcd7e14
Hossein Zaredar <hossein.zare9978@gmail.com>
2026-06-04 13:41:56 -0700

    another update to env.py

--- utils/env.py
+++ utils/env.py
@@ -6,8 +6,8 @@ import os
 
 IMAGENET_PATH = "/dev/shm/latent_cache"
 IMAGENET_CACHE_PATH = "/dev/shm/latent_cache"
-IMAGENET_FID_NPZ = "~/gcs/stats/imagenet_256_fid_stats.npz"
-IMAGENET_PR_NPZ = "~/gcs/stats/imagenet_val_prc_arr0.npz"
+IMAGENET_FID_NPZ = "/dev/shm/stats/imagenet_256_fid_stats.npz"
+IMAGENET_PR_NPZ = "/dev/shm/stats/imagenet_val_prc_arr0.npz"
 
 HF_REPO_ID = "Goodeat/drifting"
 HF_ROOT = os.environ.get("HF_ROOT", "~/hf_cache")
