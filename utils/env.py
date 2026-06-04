e0bb2ba98630d507ce849078456d8573b1d1b45e
Hossein Zaredar <hossein.zare9978@gmail.com>
2026-06-03 19:24:55 -0700

    Change default HF_ROOT path to use ~ directory

--- utils/env.py
+++ utils/env.py
@@ -10,4 +10,4 @@ IMAGENET_FID_NPZ = "/dev/shm/drift-497721-data/stats/imagenet_256_fid_stats.npz"
 IMAGENET_PR_NPZ = "/dev/shm/drift-497721-data/stats/imagenet_val_prc_arr0.npz"
 
 HF_REPO_ID = "Goodeat/drifting"
-HF_ROOT = os.environ.get("HF_ROOT", "/home/hossein/hf_cache")
+HF_ROOT = os.environ.get("HF_ROOT", "~/hf_cache")
